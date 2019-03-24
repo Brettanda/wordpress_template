d6126e842754ba9451c3675421d55bfd1b943341
Brett Anda <brettanda12@gmail.com>
2019-03-24 01:09:14 -0600

    Added the copy right symbol

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 </div>
 	<footer class="main-footer">
-		<?php print date('Y'); ?> Brett Anda. All Rights Reserved.</p>
+		<?php print date('Y'); ?> &copy; Brett Anda. All Rights Reserved.</p>
 	</footer>
 <?php wp_footer(); ?>
 </body>
