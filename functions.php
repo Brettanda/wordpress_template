dac14d4e3ea2907069ef3f64095aef1932515400
Brett Anda <brettanda12@gmail.com>
2019-03-24 01:07:43 -0600

    Fixed the script locations

--- functions.php
+++ functions.php
@@ -13,7 +13,7 @@
 
 //https://developer.wordpress.org/reference/functions/add_theme_support/#custom-logo
 add_theme_support('title-tag');
-add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption' ) );
+add_theme_support( 'html5', array( /*'comment-list', 'comment-form', 'search-form', */'gallery', 'caption' ) );
 add_theme_support( 'custom-header', array(
 	'header-text' => false
 ));
@@ -27,8 +27,8 @@ add_theme_support( 'custom-logo' );
 
 function linked_assets(){
 	wp_enqueue_style('theme_styles',get_stylesheet_uri());
-	wp_enqueue_script('theme_script', get_template_directory_uri() . 'scripts/scripts.js');
-	wp_enqueue_script('animejs', get_template_directory_uri() . '/assets/modules/anime-master/lib/anime.min.js');
+	wp_enqueue_script('theme_script', get_stylesheet_directory_uri() . '/assets/scripts/scripts.js');
+	wp_enqueue_script('animejs', get_stylesheet_directory_uri() . '/assets/modules/anime-master/lib/anime.min.js');
 	wp_enqueue_style('google-fonts','https://fonts.googleapis.com/css?family=Montserrat');
 }
 add_action('wp_enqueue_scripts','linked_assets');
